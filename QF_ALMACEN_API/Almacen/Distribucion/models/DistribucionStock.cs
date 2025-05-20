d140d06d85065aef5b16733c8cf3bf77769bd8c1
earoncal <angeloroncal@outlook.com>
2025-05-20 00:57:47 -0500

    AVANCES DISTRIBUCION

--- QF_ALMACEN_API/Almacen/Distribucion/models/DistribucionStock.cs
+++ QF_ALMACEN_API/Almacen/Distribucion/models/DistribucionStock.cs
@@ -5,6 +5,7 @@
         public int idproducto { get; set; }
         public string codigoproducto { get; set; } = "NINGUNO";
         public string nombre_producto { get; set; } = string.Empty;
+        public int idlaboratorio { get; set; }
         public string laboratorio { get; set; } = "NINGUNO";
         public decimal stock { get; set; }
         public string ventas { get; set; } = "A";
